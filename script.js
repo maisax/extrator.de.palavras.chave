496c333c65239470688d4ac8cf138a19262ffe2f
maisax <maisa.catunda@escola.pr.gov.br>
2025-06-16 09:31:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
-
 function mostraPalavrasChave() {
       const texto = document.querySelector('#entrada-de-texto').value;
           const campoResultado = document.querySelector('#resultado-palavrachave');
+          campoResultado.textContent = texto;
 }
