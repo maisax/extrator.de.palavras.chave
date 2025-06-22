ed9749d2a2c532c65a524c77753dbfc15f1df8ec
maisax <maisa.catunda@escola.pr.gov.br>
2025-06-22 12:34:41 -0300

    .

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ function mostraPalavrasChave() {
       const texto = document.querySelector('#entrada-de-texto').value;
        const campoResultado = document.querySelector('#resultado-palavrachave');
      const palavrasChave = processaTexto(texto);
+
+
       campoResultado.textContent = palavrasChave.join(", ");
  }
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
-
-     contaFrequencias(palavras)
+    const frequencias = contaFrequencias(palavras);
      return palavras;
     }
     
